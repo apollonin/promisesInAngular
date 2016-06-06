75803be3d0baee896388c54b50dd5e51b5ffe56a
Denis Nizhegorodcev <dn@century.one>
2016-06-06 11:45:17 +0300

    remove extra code

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,7 +4,6 @@ app.controller('Ctrl', ['$scope', '$interval', '$q', function($scope, $interval,
 
 	$scope.randomWorkProgress = 'Working';
 	$scope.finished           = false;
-	var deferred = $q.defer();
 
 	function randomWork(){
 		var count = Math.floor(Math.random() * 6 + 5);
